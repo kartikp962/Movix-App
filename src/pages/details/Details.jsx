e1de5f4cb30cc971feeb57be01bc6aef64ea9892
kartikp962 <kpal87332@gmail.com>
2023-06-30 07:57:07 +0530

    small typo errors fixed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
-import Cast from './cast/cast';
+import Cast from './cast/Cast';
 import VideosSection from './videosSection/VideosSection';
 import Similar from './carousels/Similar';
 import Recommendation from './carousels/Recommendation';
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -6,7 +6,7 @@ import "./style.scss";
 
 import useFetch from "../../../hooks/useFetch";
 
-import Img from "../../../components/lazyLoadImage/img";
+import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 function HeroBanner() {
